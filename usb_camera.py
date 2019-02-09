de1adb001256e6b3ca5fd7b11c9524c44607f803
philstenning <philstenning@hotmail.com>
2019-02-09 13:28:58 +0000

    add smile

--- usb_camera.py
+++ usb_camera.py
@@ -4,6 +4,7 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -18,9 +19,12 @@ while(True):
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        # eyes = eye_cascade.detectMultiScale(roi_gray)
-        # for (ex, ey, ew, eh) in eyes:
-        #     cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        smile = smile_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in smile:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
